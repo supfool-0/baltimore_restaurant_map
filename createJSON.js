1629830a145956c9f632d3ad06d4f891a328dbfd
jeri <diltsjeri@gmail.com>
2020-11-11 18:32:12 -0500

    switched to mapquest api - cause it's free

--- createJSON.js
+++ createJSON.js
@@ -1,4 +1,4 @@
-const geoEndpoint = `https://data.baltimorecity.gov/resource/k5ry-ef3g.json`;
+const baltimoreOpenDataEndpoint = `https://data.baltimorecity.gov/resource/k5ry-ef3g.json`;
 
 
 
@@ -17,35 +17,67 @@ const getAddresses = async (endp) => {
                 restaurantDict[restaurant.name] = formattedAddy;
             })
             
-            batchCallGeocodeio(restaurantDict);
+            callMapQuest(restaurantDict);
             // makeJSON(restaurantDict);
+            // ** note: initally created the dict for geocodio. Could reconsider this structure type
 
          }) .catch ((error)=>{
             console.log(`Error: ${error}`);
         })
 }
 
-getAddresses(geoEndpoint);
+
+
+getAddresses(baltimoreOpenDataEndpoint);
 
 
 
-// batch calling geocode.io with dict obj of addresses
-const batchCallGeocodeio = async (dict) => {
+// mapQuest Developer's FREE forward geocoding (batch mode - limited to 100 address at a time)
+const callMapQuest = async (dict) => {
 
-    const YOUR_API_KEY = `f4b4fc94749253fffff5fc9a54b44ab7a7cf777`;
-    let serviceEndpoint = `https://api.geocod.io/v1.6/geocode?api_key=${YOUR_API_KEY}`;
-
-    await axios.post(serviceEndpoint, dict)
+    const YOUR_API_KEY = `8zBqKVlrb42b0WZ6bA1uCRPTm99oVacD`;
+    let serviceEndpoint = `https://www.mapquestapi.com/geocoding/v1/batch?&inFormat=kvp&outFormat=json&thumbMaps=false&maxResults=1`;
+    let len = Object.keys(dict).length; // number of address in the object dictionary =959
+    let loopCount = Math.floor(len / 100); // =9
+    let lastloopCount = (len % 100); // =59
     
-        .then((response)=>{
-            console.log(response.data);
-        }) .catch ((error)=>{
-            console.log(`Error: ${error}`);
-        })
+    // generating urls while taking into consideration the 100 address batch limit
+    Object.values(dict).forEach(function(address) {
+
+
+        let i = 0;
+        let batchURL = serviceEndpoint;
+
+        while(i<100){
+            batchURL += `&location=${address}`;
+            i++;
+        }
+
+        batchURL += `&key=${YOUR_API_KEY}`
+        // exectue API call here
+        
+
+    });
+
+
+
+    for(let i=0; i<=loopCount; i++){
+        let generatedURL = null;
+    }
+
+    // await axios.get(generatedURL)
+
+    //     .then((response) =>{
+    //         console.log(response.data);
+    //     }) .catch ((error)=>{
+    //         console.log(`Error: ${error}`);
+    //     })
 }
 
 
-
+// I like this format
+https://www.mapquestapi.com/geocoding/v1/batch?&inFormat=kvp&outFormat=json&thumbMaps=false&maxResults=1&location=Denver, 
+CO&location=1555 Blake St, Denver, CO 80202&location=Boulder&key=KEY
 
 
 
@@ -62,14 +94,3 @@ const batchCallGeocodeio = async (dict) => {
 //     let fs = require('fs'); // node.js loading module 'fs' = file system
 //     fs.writeFile("geocodeJSON_input.json", dictstring);
 // }
-
-
-
-//---------- Example curl cmd
-
-// curl -X POST \
-// -H "Content-Type: application/json" \
-// -d '["1109 N Highland St, Arlington VA", "525 University Ave, Toronto, ON, Canada", "4410 S Highway 17 92, Casselberry FL", "15000 NE 24th Street, Redmond WA", "17015 Walnut Grove Drive, Morgan Hill CA"]' \
-// https://api.geocod.io/v1.6/geocode?api_key=YOUR_API_KEY
-
-
